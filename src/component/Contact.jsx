887ad3b59647a3297d4a956bb2f96558b3fac2b8
Amanjarngal <you@example.com>
2025-01-13 01:53:31 +0530

    new

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -23,7 +23,6 @@ export default function Example() {
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Contact Me</h2>
-        <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
